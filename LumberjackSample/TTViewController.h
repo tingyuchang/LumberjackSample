44ef9639eb021ce8892a8c91f7b41b00de717528
Matt Chang <mattchang@accuvally.com>
2014-09-03 09:57:57 +0800

    add TTViewController

---
+++ LumberjackSample/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  LumberjackSample
+//
+//  Created by Matt Chang on 2014/9/3.
+//  Copyright (c) 2014年 Accuvally Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
